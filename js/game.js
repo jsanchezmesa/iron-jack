99d1b75036ecd1e373fe449e2a25ebd390052418
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-01 16:51:12 +0200

    Now the character can climb and fall on platforms

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 function Game(canvas) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext("2d");
-  
+
   this.player = new Player(this);
-  
+
   this.numPlatforms = 5;
   this.platformArray = [];
 
@@ -11,23 +11,22 @@ function Game(canvas) {
 }
 
 Game.prototype.start = function() {
-
-  this.intervalId = setInterval( function() {
-    this.clear();
-    this.move();
-    this.draw();
-    this.platformCollision();
-  }.bind(this), 1000 / 60);
-
-}
+  this.intervalId = setInterval(
+    function() {
+      this.clear();
+      this.move();
+      this.draw();
+      this.platformCollision();
+    }.bind(this),
+    1000 / 60
+  );
+};
 
 Game.prototype.pause = function() {
   clearInterval(this.intervalId);
-}
+};
 
-Game.prototype.finished = function() {
-  
-}
+Game.prototype.finished = function() {};
 
 Game.prototype.reset = function() {
   this.pause();
@@ -36,41 +35,58 @@ Game.prototype.reset = function() {
   this.platformArray = [];
   this.generatePlatforms();
   this.clear();
-}
+};
 
 Game.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-}
+};
 
 Game.prototype.draw = function() {
-  this.platformArray.forEach( function(platform) {
+  this.platformArray.forEach(function(platform) {
     platform.draw();
   });
   this.player.draw();
-}
+};
 
 Game.prototype.move = function() {
   this.player.move();
-}
+};
 
 Game.prototype.generatePlatforms = function() {
-  for(var i = 0; i < this.numPlatforms; i++) {
-    this.platformArray.push( new Platform(this) );
+  for (var i = 0; i < this.numPlatforms; i++) {
+    this.platformArray.push(new Platform(this));
   }
-}
+};
 
 Game.prototype.platformCollision = function() {
-  for(var i = 0; i < this.platformArray.length; i++) {
-    if( (this.player.x < this.platformArray[i].x + this.platformArray[i].width) &&
-        (this.player.x + this.player.width > this.platformArray[i].x) &&
-        (this.player.y < this.platformArray[i].y + this.platformArray[i].height) &&
-        (this.player.y + this.player.height > this.platformArray[i].y) ) {
-      alert("CHOCA");
-      this.reset();
+  var collision = false;
+  var platform;
+  for (var i = 0; i < this.platformArray.length; i++) {
+    if( (this.player.x <= this.platformArray[i].x + this.platformArray[i].width) &&
+    (this.player.x + this.player.width >= this.platformArray[i].x) &&
+    (this.player.y <= this.platformArray[i].y + this.platformArray[i].height) &&
+    (this.player.y + this.player.height >= this.platformArray[i].y)  ) {
+      collision = true;
+      platform = this.platformArray[i];
     }
   }
-}
 
-Game.prototype.enemyCollision = function() {
+  if( collision ) {
+    this.player.y = platform.y - this.player.height;
+    this.player.isOnPlatform = true;
+    this.player.isJumping = false;
+  } else if(!this.player.isJumping && this.player.isOnPlatform ) {
+    this.player.isJumping = true;
+    this.player.isOnPlatform = false;
+  }
 
-}
+  console.log("***************");
+  console.log("COLLISION ==> " + collision );
+  console.log("JUMPING ==> " + this.player.isJumping );
+  console.log("PLATFORM ==> " + this.player.isOnPlatform );
+  console.log("***************");
+};
+
+Game.prototype.enemyCollision = function() {};
+
+Game.prototype.itemCollision = function() {};
--- js/player.js
+++ js/player.js
@@ -6,16 +6,17 @@ function Player(game) {
   this.width = 50;
   this.height = 50;
 
-  this.originX = this.game.canvas.width/2 - this.width/2; // position on start
-  this.originY = this.game.canvas.height - this.height;   // positon on start
+  this.originX = this.game.canvas.width / 2 - this.width / 2; // position on start
+  this.originY = this.game.canvas.height - this.height; // positon on start
   this.x = this.originX;
   this.y = this.originY;
 
-  this.speed = 5;     // speed to move in x or y
-  this.dx = 0;        // distance to move in x
-  this.dy = 0;        // distance to move in y
+  this.speed = 5; // speed to move in x or y
+  this.dx = 0; // distance to move in x
+  this.dy = 0; // distance to move in y
   this.brakeX = 0.9; // brake x,0 movement
   this.isJumping = false;
+  this.isOnPlatform = false;
 
   this.gravity = 0.25;
 
@@ -24,41 +25,47 @@ function Player(game) {
 
 Player.prototype.draw = function() {
   this.game.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-}
+};
 
 Player.prototype.move = function() {
-  this.dy += this.gravity;      // y move change with gravity
-
-  if( !this.isJumping ) {
-    this.dx *= this.brakeX;     // if player is on floor, it brakes when it moves to left or right
+  // if player is on floor or a platform, it brakes when it moves to left or right
+  if (!this.isJumping || this.isOnPlatform ) {
+    this.dx *= this.brakeX;
   }
-  this.x += this.dx;            // increment movement in x
-  this.y += this.dy;            // increment movement in y
+  this.x += this.dx; // increment movement in x
+
+  // apply gravity when falling
+  this.dy += this.gravity;
+  
+  if ( this.isJumping || !this.isOnPlatform) {
+    this.y += this.dy;
+  }
 
   // check limits in x
-  if( this.x + this.width > this.game.canvas.width ) {
+  if (this.x + this.width > this.game.canvas.width) {
     this.x = this.game.canvas.width - this.width;
-  } else if( this.x <= 0 ) {
+  } else if (this.x < 0) {
     this.x = 0;
   }
 
   // check limits in y
-  if( this.y + this.height > this.game.canvas.height ) {
+  if (this.y + this.height > this.game.canvas.height) {
     this.y = this.game.canvas.height - this.height;
     this.isJumping = false;
-  } 
-}
+  }
+};
 
 Player.prototype.setListeners = function() {
   document.onkeydown = function(event) {
-    switch( event.keyCode ) {
+    switch (event.keyCode) {
       // jump
       // if player is on floor, it decrease distance (move up) in y
       case 74: // J key
       case 87: // W key
       case 38: // Up key
-        if( !this.isJumping ) {
+        if (!this.isJumping) {
           this.isJumping = true;
+          this.isOnPlatform = false;
           this.dy = -this.speed * 2;
         }
         break;
@@ -66,7 +73,7 @@ Player.prototype.setListeners = function() {
       // it applies inertia
       case 65: // A key
       case 37: // Left key
-        if( this.dx > -this.speed ) {
+        if (this.dx > -this.speed) {
           this.dx -= 2;
         }
         break;
@@ -74,18 +81,16 @@ Player.prototype.setListeners = function() {
       // it applies inertia
       case 68: // D key
       case 39: // Right key
-        if( this.dx < this.speed ) {
+        if (this.dx < this.speed) {
           this.dx += 2;
         }
         break;
     }
   }.bind(this);
-
-  document
-}
+};
 
 // reset to original position
 Player.prototype.reset = function() {
   this.x = this.originX;
   this.y = this.originY;
-}
+};
